392aab8a32b39c77412dc6fe18a1325b592735cb
Daniel-Maslyanikov <87470766+Daniel-Maslyanikov@users.noreply.github.com>
2022-05-10 19:50:04 +0300

    Add react router

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-function Categories({items, onClick,}) {
-const [activeItem, setActiveItem] = useState(null)
+function Categories({ items, onClick }) {
+  const [activeItem, setActiveItem] = useState(null);
 
-	return (
-		<div className="categories">
-		<ul>
-			<li>Все</li>
-			{items.map((name,index) => (
-				<li 
-				className={activeItem === index ? 'active' : ''} 
-				onClick = {() => setActiveItem(index)} 
-				key ={`{name}_${index}`}>
-					{name}
-					</li>
-			))}
-		</ul>
-	</div>
-	)
+  return (
+    <div className="categories">
+      <ul>
+        <li className={activeItem === null ? 'active' : ''} onClick={() => setActiveItem(null)}>
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeItem === index ? 'active' : ''}
+              onClick={() => setActiveItem(index)}
+              key={`{name}_${index}`}>
+              {name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Categories
+export default Categories;
